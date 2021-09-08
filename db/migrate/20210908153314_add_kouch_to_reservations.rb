8592ba097b9514d3f5b1ffdb72a654e0e82aa7a7
yagi1231 <at.sao.1231@gmail.com>
2021-09-09 01:49:35 +0900

    修正

--- db/migrate/20210908153314_add_kouch_to_reservations.rb
+++ db/migrate/20210908153314_add_kouch_to_reservations.rb
@@ -1,9 +1,7 @@
 class AddKouchToReservations < ActiveRecord::Migration[6.1]
   def change
-    add_column :reservations, :tel, :string
     add_column :reservations, :time, :string
-    add_column :reservations, :backtime, :string
-    add_column :reservations, :telnum, :string
     add_column :reservations, :remarks, :string
+    add_column :reservations, :tel, :string
   end
 end
