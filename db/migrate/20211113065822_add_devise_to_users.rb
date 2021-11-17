f2d13a0e5311ae65119a24d3de92a13f99b5db02
yagi1231 <at.sao.1231@gmail.com>
2021-11-18 03:21:31 +0900

    修正

--- db/migrate/20211113065822_add_devise_to_users.rb
+++ db/migrate/20211113065822_add_devise_to_users.rb
@@ -4,7 +4,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.integer :group,             null: false
