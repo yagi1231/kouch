26c86e9a06d53d06de9d8708efff41fac3194a50
yagi1231 <at.sao.1231@gmail.com>
2021-11-24 04:56:40 +0900

    deviseの修正

--- db/migrate/20211123191117_add_devise_to_users.rb
+++ db/migrate/20211123191117_add_devise_to_users.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -35,7 +35,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
