9b3b0034d3bc43eba76afc920346383cb2c5f40d
yagi1231 <at.sao.1231@gmail.com>
2021-11-24 04:34:45 +0900

    deviseの修正

--- db/migrate/20211113065822_add_devise_to_user.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-class AddDeviseToUser < ActiveRecord::Migration[6.1]
-  
-end
--- db/migrate/20211113065822_add_devise_to_users.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-class AddDeviseToUsers < ActiveRecord::Migration[6.1]
-  
-end
