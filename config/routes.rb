ea7ed8a763e36e52179b0c039a744d85e399a0a8
yagi1231 <at.sao.1231@gmail.com>
2021-11-24 05:12:54 +0900

    deviseの修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   resources :users
   resources :infos
   root 'reservations#index'
-
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
   post 'logout' => 'users#logout'
