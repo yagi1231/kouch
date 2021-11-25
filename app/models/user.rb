92b65e19a12771e211ec82075d2126d653d8dd7d
yagi1231 <at.sao.1231@gmail.com>
2021-11-26 03:29:25 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
