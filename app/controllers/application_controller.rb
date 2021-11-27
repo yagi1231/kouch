cbee4473e4baad50f03cb1bc31186ecf367deec9
yagi1231 <at.sao.1231@gmail.com>
2021-11-27 17:08:17 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
+
+  def after_sign_in_path_for(resource)
+    reservations_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path 
+  end
 end
