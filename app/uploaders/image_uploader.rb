2165b20f2e659dc83756da75972176150842fc6c
yagi1231 <at.sao.1231@gmail.com>
2021-12-04 23:26:38 +0900

    修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
   #上限変更
-    process :resize_to_limit => [700, 700]
+    process :resize_to_limit => [300, 700]
   
   #JPGで保存
     process :convert => 'jpg'
