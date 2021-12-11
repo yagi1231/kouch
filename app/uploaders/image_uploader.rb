5659c50e7c2cfa2532be3e248291b5e617d38ea7
yagi1231 <at.sao.1231@gmail.com>
2021-12-12 01:20:00 +0900

    修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,21 +8,36 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file # 本番環境以外
   end
 
-    include CarrierWave::RMagick
+  include CarrierWave::RMagick
 
-    process :resize_to_limit => [300, 700]
+  #上限変更
+    process :resize_to_limit => [00, 700]
   
+  #JPGで保存
+    process :convert => 'jpg'
+  
+  #サムネイルを生成
     version :thumb do
       process :resize_to_limit => [300, 300]
     end
   
+  # jpg,jpeg,gif,pngのみ
     def extension_white_list
       %w(jpg jpeg gif png)
     end
-
+  
+  #ファイル名を変更し拡張子を同じにする
     def filename
       super.chomp(File.extname(super)) + '.jpg' 
     end
+  
+  #日付で保存
+    def filename
+      if original_filename.present?
+        time = Time.now
+        name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+        name.downcase
+      end
   
   # Choose what kind of storage to use for this uploader:
   # Override the directory where uploaded files will be stored.
@@ -61,5 +76,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  
+end
 end
