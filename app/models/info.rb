bc057d991edf967403e6657e75ef1118956a2598
yagi1231 <at.sao.1231@gmail.com>
2021-12-22 09:53:35 +0900

    修正

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,8 +1,7 @@
 class Info < ApplicationRecord
   VALID_PHONE_NUMBER_REGEX = /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
   VALID_ADDRESS_REGER = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
-  VALID_NAME_REGER = /\p{Katakana}/
-  validates :name, presence: true, format: { with: VALID_NAME_REGER }
+  validates :name, { presence: true }
   validates :address,  presence: true, format: { with: VALID_ADDRESS_REGER }
   validates :telnum,  presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
 
