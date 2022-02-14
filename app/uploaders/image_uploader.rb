25424df90f1f41242a3504fa1dff33bde7b0fbd2
yagi1231 <at.sao.1231@gmail.com>
2022-02-14 09:13:01 +0900

    修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,20 +2,23 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  if Rails.env.production?
     storage :fog # 本番環境のみ
- 
+  else
+    storage :file # 本番環境以外
+  end
+
   include CarrierWave::RMagick
 
   #上限変更
-    process :resize_to_limit => [170, 150]
+    process :resize_to_limit => [170, 195]
   
   #JPGで保存
     process :convert => 'jpg'
   
   #サムネイルを生成
     version :thumb do
-      process :resize_to_limit => [170, 150]
+      process :resize_to_limit => [170, 195]
     end
   
   # jpg,jpeg,gif,pngのみ
