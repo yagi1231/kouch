24802fcdfe5c10be5b8ad5e7dbcb583e460379ba
yagi1231 <at.sao.1231@gmail.com>
2022-02-17 03:56:32 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_155610) do
+ActiveRecord::Schema.define(version: 2022_02_15_171613) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_155610) do
     t.string "category"
     t.string "categoryname"
     t.string "image"
+    t.string "pri"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
